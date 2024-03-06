36dee1a91a5086d164278eeca28a55f947468c44
unknown <vgudojt@mail.ru>
2024-03-06 13:50:34 +0300

    change function AddPost in App.js

--- src/App.js
+++ src/App.js
@@ -9,29 +9,28 @@ import { MyInput } from "./components/UI/input/MyInput";
 function App() {
   const [posts, setPosts] = useState([{ id: 1, title: "js", body: "dfdfxfv" },
   { id: 2, title: "css", body: "gg" }]);
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
-
+  // const [title, setTitle] = useState('')
+  // const [body, setBody] = useState('')
+const [post, setPost] = useState([{title: '', body: ''}])
 
 
   const addNewPost = (event) => {
     event.preventDefault()
-const newPost = {
-  id: Date.now(),
-  title: title,
-  body: 'fbngv'
-}
-setPosts([...posts, newPost])
-setTitle('')
-setBody('')
+// const newPost = {
+//   id: Date.now(),
+//   title: post.title,
+//   body: 'fbngv'
+// }
+setPosts([...posts, {...post, id: Date.now()}])
+setPost({ title: '', body: ''})
 
   }
 
   return (
     <div>
       <form>
-        <MyInput  value={title} onChange={({target}) => setTitle(target.value)} type="text" placeholder="Название поста"/>
-        <MyInput value={body} onChange={({target}) => setBody(target.value)} type="text" placeholder="Описание поста"/>
+        <MyInput  value={post.title} onChange={({target}) => setPost({...post, title: target.value})} type="text" placeholder="Название поста"/>
+        <MyInput value={post.body} onChange={({target}) => setPost({...post, body: target.value})} type="text" placeholder="Описание поста"/>
 <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
    <PostList post={posts}/>
