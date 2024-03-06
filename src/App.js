ec2ed41450dabc9df7a8c85621512f5f56251b93
unknown <vgudojt@mail.ru>
2024-03-06 16:16:50 +0300

    add MySelect in App.jsx

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { PostList } from "./components/PostList";
 import { MyButton } from "./components/UI/button/MyButton";
 import { MyInput } from "./components/UI/input/MyInput";
 import { PostForm } from "./components/PostForm";
+import { MySelect } from "./components/UI/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([{ id: 1, title: "js", body: "dfdfxfv" },
   { id: 2, title: "css", body: "gg" }]);
+  const [selectedSort, setSelectedSort] = useState('')
 
   const createPost = (newPost) => {
 setPosts([...posts, newPost])
@@ -20,9 +22,16 @@ setPosts([...posts, newPost])
     setPosts(newPosts)
   }
 
+  const sortPosts = (sort) => {
+   setSelectedSort(sort);
+   setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     <div>
 <PostForm create={createPost}/>
+<hr style={{margin: '15px 0'}}/>
+<MySelect value={selectedSort} onChange={sortPosts} defaultValue='Сортировка' options={[{value: 'title', name: 'По названию'}, {value: 'body', name: 'По описанию'}]}/>
 {posts.length ? <PostList deletePost={deletePost} post={posts}/> : <div>Посты не найдены</div>}
    
     </div>
