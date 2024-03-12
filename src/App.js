ca468415b0e588853d3c8ab134c8f4d933c0c4f0
unknown <vgudojt@mail.ru>
2024-03-12 15:36:50 +0300

    add Axios and Loader in App.jsx

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { useMemo, useRef, useState } from "react";
-import { Counter } from "./components/Counter";
-import { CounterClass } from "./components/CounterClass";
-import { PostItem } from "./components/PostItem";
+import { useEffect, useState } from "react";
 import { PostList } from "./components/PostList";
-import { MyButton } from "./components/UI/button/MyButton";
-import { MyInput } from "./components/UI/input/MyInput";
 import { PostForm } from "./components/PostForm";
-import { MySelect } from "./components/UI/select/MySelect";
 import { PostFilter } from "./components/PostFilter";
 import { MyModal } from "./components/UI/MyModal/MyModal";
 import { usePosts } from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import { Loader } from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 function App() {
   const [posts, setPosts] = useState([{ id: 1, title: "js", body: "dfdfxfv" },
   { id: 2, title: "css", body: "gg" }]);
 const [filter, setFilter] = useState({sort: '', query: ''})
 const [modal, setModal] = useState(false)
+const [fetchPosts, isPostsLoading, postError] = useFetching(async() => {
+  const posts = await PostService.getAll()
+  setPosts(posts)
+})
 
 const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
@@ -24,7 +25,9 @@ setPosts([...posts, newPost])
 setModal(false)
   }
 
-
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   const deletePost = (id) => {
     const newPosts = posts.filter(post => post.id !== id)
@@ -34,12 +37,15 @@ setModal(false)
 
   return (
     <div>
+    <button onClick={fetchPosts}>GET POSTS</button>
       <button style={{marginTop: 30}} onClick={() => setModal(true)}>Создать пользователя</button>
       <MyModal visible={modal} setVisible={setModal}><PostForm create={createPost}/></MyModal>
 
 <hr style={{margin: '15px 0'}}/>
 <PostFilter filter={filter} setFilter={setFilter}/>
-{<PostList deletePost={deletePost} post={sortedAndSearchedPosts}/>}
+{postError && <h1>Произошла ошибка ${postError}</h1>}
+{isPostsLoading ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}> <Loader/></div> : <PostList deletePost={deletePost} post={sortedAndSearchedPosts}/>}
+
  
     </div>
   );
