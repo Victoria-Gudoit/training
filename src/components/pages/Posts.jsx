8cfa5c55a611010d8f52f686008585898718bdbb
unknown <vgudojt@mail.ru>
2024-03-26 15:49:37 +0300

    add endless feed

--- src/components/pages/Posts.jsx
+++ src/components/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import cl from "../../styles/module.style.css"
 
@@ -12,6 +12,8 @@ import { Loader } from "../UI/Loader/Loader";
 import { PostForm } from "../PostForm";
 import PostService from "../../API/PostService";
 import { useFetching } from "../../hooks/useFetching";
+import { useObsorver } from "../../hooks/useObserver";
+import { MySelect } from "../UI/select/MySelect";
 
 function Posts() { //не смотреть 
   const [posts, setPosts] = useState([{ id: 1, title: "js", body: "dfdfxfv" },
@@ -21,11 +23,11 @@ const [modal, setModal] = useState(false)
 const [totalPages, setTotalPages] = useState(0)
 const [limit, setLimit] = useState(10)
 const [page, setPage] = useState(1)
-
+const lastElement = useRef()
 
 const [fetchPosts, isPostsLoading, postError] = useFetching(async(limit, page) => {
   const response = await PostService.getAll(limit, page)
-  setPosts(response.data)
+  setPosts([...posts, ...response.data])
   const totalCount = response.headers['x-total-count']
   setTotalPages(getPageCount(totalCount, limit))
 })
@@ -37,9 +39,13 @@ setPosts([...posts, newPost])
 setModal(false)
   }
 
+useObsorver(lastElement, page < totalPages, isPostsLoading, () => {
+  setPage(page + 1)
+})
+
   useEffect(() => {
     fetchPosts(limit, page)
-  }, [])
+  }, [page, limit])
 
   const deletePost = (id) => {
     const newPosts = posts.filter(post => post.id !== id)
@@ -48,7 +54,6 @@ setModal(false)
 
   const changePage = (page) => {
     setPage(page)
-    fetchPosts(limit, page)
   }
 
   return (
@@ -59,8 +64,18 @@ setModal(false)
 
 <hr style={{margin: '15px 0'}}/>
 <PostFilter filter={filter} setFilter={setFilter}/>
+<MySelect value={limit} onChange={value => setLimit(value)} defaultValue="Кол-во элементов на странице" options={[
+  {value: 5, name: '5'},
+  {value: 10, name: '10'},
+  {value: 5, name: '25'},
+  {value: -1, name: 'Показать все'},
+
+
+]}/>
 {postError && <h1>Произошла ошибка ${postError}</h1>}
-{isPostsLoading ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}> <Loader/></div> : <PostList deletePost={deletePost} post={sortedAndSearchedPosts}/>}
+<PostList deletePost={deletePost} post={sortedAndSearchedPosts}/>
+<div ref={lastElement} style={{height: 20, background: 'red'}}/>
+{isPostsLoading && <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}> <Loader/></div>}  
 <Pagination page={page} changePage={changePage} totalPages={totalPages}/>
 
  
